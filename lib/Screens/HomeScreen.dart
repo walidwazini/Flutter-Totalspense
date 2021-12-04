c56eeb9a0e4f1784c1f8987bb12a66305f579022
Walid Wazini <wazini25@gmail.com>
2021-12-04 20:32:32 +0800

    updates

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -35,11 +35,45 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             Column(
-              children: [
-                Card(
-                  child: Text('Transaction'),
-                ),
-              ],
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                        child: Text(
+                          ' RM ${tx.amount.toString()} ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              tx.title!,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17
+                              ),
+                            ),
+                            Text(tx.date.toString(),
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15
+                              ),
+                            )
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ),
