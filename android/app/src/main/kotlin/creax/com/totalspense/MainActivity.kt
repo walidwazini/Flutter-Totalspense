6aae6ae582e2f94648e1e7a2735f5e17eba0d94f
Walid Wazini <wazini25@gmail.com>
2021-12-04 15:01:19 +0800

    first commit

---
+++ android/app/src/main/kotlin/creax/com/totalspense/MainActivity.kt
@@ -0,0 +1,6 @@
+package creax.com.totalspense
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:totalspense/Screens/HomeScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Totalspens',
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
