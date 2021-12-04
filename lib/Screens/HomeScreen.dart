70e11162e11922f3b8ff01cffcb2db8b07f2f070
Walid Wazini <wazini25@gmail.com>
2021-12-04 15:41:32 +0800

    2nd commit

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
 
+import '../Models/TransactionModel.dart';
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final List<TransactionModel> transactions = [
+    TransactionModel(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.20,
+      date: DateTime.now(),
+    ),
+    TransactionModel(
+      id: 't1',
+      title: 'Beli tempeh',
+      amount: 12.40,
+      date: DateTime.now(),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: Column(
-        children: [
-          Text('Hello'),
-        ],
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              elevation: 10,
+              child: Container(
+                width: 200,
+                child: Text('Chart'),
+              ),
+            ),
+            Column(
+              children: [
+                Card(
+                  child: Text('Transaction'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
